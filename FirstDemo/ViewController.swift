1d23e8f831b0cf4a33ace2f3013fd8be11dab0e2
Michael Luo <michael.luo0801@gmail.com>
2018-10-24 20:24:27 +1100

    Initial Commit

---
+++ FirstDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstDemo
+//
+//  Created by Michael Luo on 24/10/18.
+//  Copyright © 2018 Michael Luo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
