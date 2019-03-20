2c8765adec7e9ca93099d2a44b7fc9dcff821fc3
Michael Luo <michael.luo0801@gmail.com>
2019-03-20 23:21:01 +1100

    add RatingControl

---
+++ FirstDemo/RatingControl.swift
@@ -0,0 +1,26 @@
+//
+//  RatingControl.swift
+//  FirstDemo
+//
+//  Created by Michael Luo on 20/3/19.
+//  Copyright © 2019 Michael Luo. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
